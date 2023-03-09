10728b4cf94097d8145e6c394f084c22c51df4f7
soroushJuly <soroushjuly@gmail.com>
2023-03-09 20:21:58 +0330

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ApplePicker
+
+Developed with Unreal Engine 5
---
+++ Source/ApplePicker/ApplePickerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ApplePickerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APPLEPICKER_API AApplePickerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
